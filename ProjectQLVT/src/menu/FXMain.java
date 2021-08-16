4b537655fdee51395edba27f8a53d41d9db5b369
thuyvie <thuyvy1235789@gmail.com>
2021-08-16 13:34:43 +0700

    remake

---
+++ ProjectQLVT/src/menu/FXMain.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package menu;
+
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+import javafx.stage.Stage;
+
+
+/**
+ *
+ * @author ASUS
+ */
+public class FXMain extends Application {
+    double x,y;
+    
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/view/MenuUI.fxml"));      
+        Scene scene = new Scene(root);
+
+        
+        root.setOnMousePressed(event -> {
+            x = event.getSceneX();
+            y = event.getSceneY();
+        });
+        
+        root.setOnMouseDragged(event -> {
+        primaryStage.setX(event.getScreenX() - x);
+        primaryStage.setY(event.getScreenY() - y);
+        });
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
---
+++ ProjectQLVT/src/menu/MenuUIController.java
@@ -0,0 +1,84 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package menu;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+
+
+/**
+ * FXML Controller class
+ *
+ * @author ASUS
+ */
+public class MenuUIController implements Initializable {
+
+    private AnchorPane contentArea;
+    @FXML
+    private BorderPane boderPane;
+    
+
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
+    }    
+
+    
+//    public void DashBoard(javafx.event.ActionEvent actionEvent) throws IOException {
+//        Parent fxml = FXMLLoader.load(getClass().getResource("DashBoard.fxml"));  
+//        contentArea.getChildren().removeAll();
+//        contentArea.getChildren().setAll(fxml);
+//    }
+
+
+    @FXML
+    private void DashBoard(MouseEvent event) throws IOException {
+        boderPane.setCenter(null);
+    }
+
+    @FXML
+    private void page1(MouseEvent event) throws IOException {
+        loadUI("Order");
+    }
+
+    @FXML
+    private void page2(MouseEvent event) throws IOException {
+                loadUI("page2");
+
+    }
+
+    @FXML
+    private void page3(MouseEvent event) throws IOException {
+        loadUI("page3");
+
+    }
+    
+    private void loadUI(String page){
+        Parent root = null;
+        
+        try {
+            root = FXMLLoader.load(getClass().getResource(page+".fxml"));
+                    } catch (IOException ex) {
+            Logger.getLogger(MenuUIController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        boderPane.setCenter(root);
+    }
+    
+}
