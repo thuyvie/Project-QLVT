d11766413f2cafc6a9aedd941a554bfbff52ba9f
thuyvie <thuyvy1235789@gmail.com>
2021-08-30 13:32:50 +0700

    up login

--- ProjectQLVT/src/controller/LoginController.java
+++ ProjectQLVT/src/controller/LoginController.java
@@ -7,31 +7,29 @@ package controller;
 
 import com.jfoenix.controls.JFXButton;
 import java.io.IOException;
-import java.util.logging.Level;
+
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-//import javafx.scene.Scene;
+import javafx.scene.Scene;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
-//import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
-//import javafx.stage.Stage;
-//import javax.swing.JOptionPane;
-//import java.sql.PreparedStatement;
-//import java.sql.DriverManager;
-//import java.sql.ResultSet;
-//import java.sql.*;
-//import javafx.util.Duration;
-//import tray.animations.AnimationType;
-//import tray.notification.NotificationType;
-//import tray.notification.TrayNotification;
-
+import javax.swing.JOptionPane;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javafx.scene.Parent;
+import javafx.stage.Stage;
 /**
  * FXML Controller class
  *
@@ -49,10 +47,14 @@ public class LoginController implements Initializable {
     private PasswordField tfpassword;
     @FXML
     private JFXButton btnlogin;
+    
+    Connection conn;
+    PreparedStatement pst;
+    ResultSet rs;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+       
     }
 
     @FXML
@@ -61,81 +63,53 @@ public class LoginController implements Initializable {
     }
 
     @FXML
-    private void handleButtonAction(ActionEvent event) {
+    private void login(ActionEvent event) throws SQLException, IOException {
+        String username = tfusername.getText();
+        String pass = tfpassword.getText();
+        
+        if (username.equals("") && pass.equals("")) {
+            
+            JOptionPane.showMessageDialog(null, "UserName or Password Blank");
+        }
+        else{
+            try {
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/qlvt?useSSL=false", "root","");
+                
+                pst = conn.prepareStatement("select * from emp where Account = ? and Password = ?");
+                pst.setString(1, username);
+                pst.setString(2, pass);
+                
+                rs = pst.executeQuery();
+                
+                if (rs.next()) {
+
+                    
+                    btnlogin.getScene().getWindow().hide();
+                    Parent root = FXMLLoader.load(getClass().getResource("/view/MenuUI.fxml"));
+                    Stage stage = new Stage();
+                    Scene scene = new Scene(root);
+                    stage.setScene(scene);
+                    stage.show();
+                    
+                }
+                else{
+                   JOptionPane.showMessageDialog(null, "Login Failed"); 
+                   
+                   tfusername.setText("");
+                   tfpassword.setText("");
+                   tfusername.requestFocus();
+                }
+            
+            } catch (ClassNotFoundException ex) {
+                Logger.getLogger(LoginController.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (SQLException ex){
+                Logger.getLogger(LoginController.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
     }
 
-    /**
-     * Initializes the controller class.
-     */
-//    Connection conn;
-//    PreparedStatement pst;
-//    ResultSet rs;
-//    @Override
-//    public void initialize(URL url, ResourceBundle rb) {
-//        // TODO
-//    }    
-//
-//    @FXML
-//    private void Close(MouseEvent event) {
-//        Stage stage = (Stage)panel.getScene().getWindow();
-//        stage.close();
-//    }
-//    @FXML
-//    private void handleButtonAction(ActionEvent event) throws IOException {
-//        String username = tfusername.getText();
-//        String pass = tfpassword.getText();
-//
-//        if (username.equals("") && pass.equals("")) {
-//            JOptionPane.showMessageDialog(null, "User or Password Blank");
-//        } else {
-//            try {
-//                Class.forName("com.mysql.cj.jdbc.Driver");
-//                String url = "jdbc:mysql://localhost:3306/crm?useSSL=false";
-//                String user = "root";
-//                String password = "";
-//                Connection conn = (Connection) DriverManager.getConnection(url, user, password);
-//
-//                pst = conn.prepareStatement("select * from employee where AccountEmployee =? and PassEmployee =?");
-//                pst.setString(1, username);
-//                pst.setString(2, pass);
-//
-//                rs = pst.executeQuery();
-//                if (rs.next()) {
-//                    Stage window = (Stage) this.panel.getScene().getWindow();
-//                    window.setScene(new Scene(FXMLLoader.load(this.getClass().getResource("/FXML/Dashboard.fxml"))));
-//                    window.centerOnScreen();
-//                    String tilte = "Sign In";
-//                    String message = "WELCOME TO CRM MANEGMET SYSTEM ";
-//                    tray.notification.TrayNotification tray = new TrayNotification();
-//                    AnimationType type = AnimationType.POPUP;
-//                    tray.setAnimationType(type);
-//                    tray.setTitle(tilte);
-//                    tray.setMessage(message);
-//                    tray.setNotificationType(NotificationType.SUCCESS);
-//                    tray.showAndDismiss(Duration.millis(3000));
-//
-//                } else {
-//                    String tilte = "Sign In";
-//                    String message = "UserName or Password failed";
-//                    tray.notification.TrayNotification tray = new TrayNotification();
-//                    AnimationType type = AnimationType.POPUP;
-//
-//                    tray.setAnimationType(type);
-//                    tray.setTitle(tilte);
-//                    tray.setMessage(message);
-//                    tray.setNotificationType(NotificationType.INFORMATION);
-//                    tray.showAndDismiss(Duration.millis(3000));
-//                    tfusername.setText("");
-//                    tfpassword.setText("");
-//                    tfusername.requestFocus();
-//                }
-//
-//            } catch (ClassNotFoundException ex) {
-//                java.util.logging.Logger.getLogger(LoginController.class.getName()).log(Level.SEVERE, null, ex);
-//            } catch (SQLException ex) {
-//                java.util.logging.Logger.getLogger(LoginController.class.getName()).log(Level.SEVERE, null, ex);
-//            }
-//        }
-//        
-//    }
+   
+
+
 }
